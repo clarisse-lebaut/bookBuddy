b0bd6cc7098890939400d45f12ec2827163dbbba
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-05-24 11:25:51 +0200

    create all roots about users parts

--- api/server.js
+++ api/server.js
@@ -1,16 +1,23 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
+// require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const userRouter = require("./rootsUser.js");
 
 const app = express();
-const port = 3000;
+const port = 2024;
+
+app.use(express.json());
+app.use("/user", userRouter);
 
 mongoose.connect(`mongodb://localhost:27017/BookBuddy`);
 
 const db = mongoose.connection;
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to the database!'));
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to the database!"));
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+// Exportation du serveur
+module.exports = app;
