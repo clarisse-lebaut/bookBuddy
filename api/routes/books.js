9ec0aac8937be30769b5272ff5527a3e37812ca1
jeremy-gregoire <jeremy.gregoire@laplateforme.io>
2024-05-27 10:04:54 +0200

    save to dev

--- api/routes/books.js
+++ api/routes/books.js
@@ -1,19 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const User = require('../models/user');
+const Book = require('../models/book');
 
 const router = express.Router();
 
 // Route that allows you to get a list of books of a user's collections
-router.get('/', async (_, response) => {
+router.get('/id?', async (request, response) => {
+  console.log('lol');
   try {
-    const user = await User.findOne({ _id: request.body.userId });
-    if (user === null) {
-      return response.status(404).json({
-        message: "User doesn't exist in the database.",
+    if (request.params.id) {
+      const user = await User.findOne({
+        _id: mongoose.Types.ObjectId.createFromHexString(request.params.id),
       });
-    }
 
-    response.status(200).json(user.collections);
+      if (user === null) {
+        return response.status(404).json({
+          message: "User doesn't exist in the database.",
+        });
+      }
+
+      return response.status(200).json(user.collections);
+    } else {
+      const books = await Book.find({});
+      return response.status(200).json(books);
+    }
   } catch (error) {
     response.status(404).json({ message: error.message });
   }
--- api/server.js
+++ api/server.js
@@ -1,40 +1,21 @@
-// require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const userRouter = require("./rootsUser.js");
+
+const booksRoute = require('./routes/books');
 
 const app = express();
-const port = 2024;
-
-app.use(express.json());
-app.use("/user", userRouter);
+const port = 3000;
 
 mongoose.connect(`mongodb://localhost:27017/BookBuddy`);
+
+app.use(express.json());
 
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to the database!'));
 
-app.use(express.json());
-
-const addUserRoute = require('./routes/addUser');
-app.use('/api/addUser', addUserRoute);
-
-const addBookRoute = require('./routes/addBook');
-app.use('/api/addBook', addBookRoute);
-
-const booksRoute = require('./routes/books');
 app.use('/api/books', booksRoute);
-
-const bookRoute = require('./routes/book');
-app.use('/api/book', bookRoute);
-
-const rewardRoute = require('./routes/reward');
-app.use('/api/reward', rewardRoute);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
-
-// Exportation du serveur
-module.exports = app;
