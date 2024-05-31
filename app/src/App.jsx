94254b127b45209b38b29a43e1407329571812e9
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-05-31 09:38:10 +0200

    add roots log and some css + direction on home page

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import LogIn from './pages/LogIn';
+import LogUp from './pages/LogUp';
 
 export default function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path="/" element={<Home />} />
+        {/* route pour se connecter */}
+        <Route path="/signIn" element={<LogIn />} />
+        {/* route pour se crée un compte */}
+        <Route path="/signUp" element={<LogUp />} />
       </Routes>
     </>
   );
---
+++ app/src/pages/LogIn.jsx
@@ -0,0 +1,89 @@
+import { Button } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import "./logStyle.css";
+
+export default function LogIn() {
+  const [form, setForm] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [response, setResponse] = useState(null);
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch("http://localhost:3000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form),
+    })
+      .then((response) => response.text())
+      .then((data) => {
+        if (data === "Login successful") {
+          // La connexion a réussi, vous pouvez rediriger l'utilisateur vers une autre page, par exemple
+          window.location.href = "/";
+        } else {
+          // La connexion a échoué, vous pouvez afficher un message d'erreur
+          setErrorMessage(data.message);
+        }
+      })
+      .catch((error) => console.error("Error:", error));
+  };
+
+  return (
+    <>
+      {errorMessage && (
+        <div className="alert alert-danger" role="alert">
+          {errorMessage}
+        </div>
+      )}
+      <h1>SE CONNECTER</h1>
+      <Form className="d-flex flex-column justify-content-center">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>
+            <p>User Name</p>
+          </Form.Label>
+          <Form.Control
+            className="input-style shadow-lg"
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={handleChange}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>
+            <p>Password</p>
+          </Form.Label>
+          <Form.Control
+            className="input-style"
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Button className="button-style" type="submit" onClick={handleSubmit}>
+          <p>Submit</p>
+        </Button>
+        <p> Pas encore inscrit ?</p>
+        <Link to="/signUp">Créez vous un compte !</Link>
+      </Form>
+    </>
+  );
+}
