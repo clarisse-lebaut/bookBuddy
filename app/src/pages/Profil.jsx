6f8799add542838e1de0e42ab4b5ac7a9bedc496
Enzo-Lorenzelli <enzo.lorenzelli@laplateforme.io>
2024-06-01 10:51:52 +0200

    profil page style added

--- app/src/pages/Profil.jsx
+++ app/src/pages/Profil.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useContext } from "react";
-import NavBar from "../../component/nav";
-import { Button } from "react-bootstrap";
-import { useParams } from "react-router-dom"; // Ajouter cette ligne
+import React, { useState, useContext } from 'react';
+import NavBar from '../../component/nav';
+import { Button } from 'react-bootstrap';
+import { useParams } from 'react-router-dom'; // Ajouter cette ligne
+import './Profil.css'; // Importer le fichier CSS
 
 export default function Profil() {
-  const [password, setPassword] = useState("");
+  const [password, setPassword] = useState('');
 
   // Récupérer l'ID de l'utilisateur dans l'URL en utilisant le hook useParams
   const { id } = useParams();
@@ -26,9 +27,9 @@ export default function Profil() {
 
     // Envoyer la requête PUT à l'API
     const response = await fetch(url, {
-      method: "PUT",
+      method: 'PUT',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ newPassword: password }),
     });
@@ -44,14 +45,18 @@ export default function Profil() {
   return (
     <>
       <NavBar />
-      <form onSubmit={handleSubmit}>
-        <input
-          type="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-        <Button type="submit">Modifier le mot de passe</Button>
-      </form>
+      <div className='profile-container'>
+        <div className='username'>Pseudo de l'utilisateur : ""</div>
+        <form onSubmit={handleSubmit} className='password-form'>
+          <input
+            type='password'
+            placeholder='Nouveau mot de passe'
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <Button type='submit'>Modifier le mot de passe</Button>
+        </form>
+      </div>
     </>
   );
 }
